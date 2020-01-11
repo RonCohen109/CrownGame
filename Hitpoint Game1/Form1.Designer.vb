853e788d9b3c03e9ff02e0b9e9732573e591498e
RonCohen109 <ronnamdar109@gmail.com>
2020-01-11 19:02:05 +0200

    Add files via upload

---
+++ Hitpoint Game1/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.player1 = New System.Windows.Forms.PictureBox()
+        Me.opponent1 = New System.Windows.Forms.PictureBox()
+        Me.player2 = New System.Windows.Forms.PictureBox()
+        Me.player3 = New System.Windows.Forms.PictureBox()
+        Me.opponent2 = New System.Windows.Forms.PictureBox()
+        Me.opponent3 = New System.Windows.Forms.PictureBox()
+        Me.plus1scorerace = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.NewGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.player1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.opponent1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.player2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.player3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.opponent2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.opponent3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.plus1scorerace, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'player1
+        '
+        Me.player1.BackColor = System.Drawing.Color.MediumBlue
+        Me.player1.Location = New System.Drawing.Point(12, 452)
+        Me.player1.Name = "player1"
+        Me.player1.Size = New System.Drawing.Size(35, 30)
+        Me.player1.TabIndex = 0
+        Me.player1.TabStop = False
+        '
+        'opponent1
+        '
+        Me.opponent1.BackColor = System.Drawing.Color.Red
+        Me.opponent1.Location = New System.Drawing.Point(12, 58)
+        Me.opponent1.Name = "opponent1"
+        Me.opponent1.Size = New System.Drawing.Size(35, 30)
+        Me.opponent1.TabIndex = 1
+        Me.opponent1.TabStop = False
+        '
+        'player2
+        '
+        Me.player2.BackColor = System.Drawing.Color.MediumBlue
+        Me.player2.Location = New System.Drawing.Point(374, 452)
+        Me.player2.Name = "player2"
+        Me.player2.Size = New System.Drawing.Size(35, 30)
+        Me.player2.TabIndex = 2
+        Me.player2.TabStop = False
+        '
+        'player3
+        '
+        Me.player3.BackColor = System.Drawing.Color.MediumBlue
+        Me.player3.Location = New System.Drawing.Point(753, 452)
+        Me.player3.Name = "player3"
+        Me.player3.Size = New System.Drawing.Size(35, 30)
+        Me.player3.TabIndex = 3
+        Me.player3.TabStop = False
+        '
+        'opponent2
+        '
+        Me.opponent2.BackColor = System.Drawing.Color.Red
+        Me.opponent2.Location = New System.Drawing.Point(374, 58)
+        Me.opponent2.Name = "opponent2"
+        Me.opponent2.Size = New System.Drawing.Size(35, 30)
+        Me.opponent2.TabIndex = 4
+        Me.opponent2.TabStop = False
+        '
+        'opponent3
+        '
+        Me.opponent3.BackColor = System.Drawing.Color.Red
+        Me.opponent3.Location = New System.Drawing.Point(753, 58)
+        Me.opponent3.Name = "opponent3"
+        Me.opponent3.Size = New System.Drawing.Size(35, 30)
+        Me.opponent3.TabIndex = 5
+        Me.opponent3.TabStop = False
+        '
+        'plus1scorerace
+        '
+        Me.plus1scorerace.BackColor = System.Drawing.Color.Green
+        Me.plus1scorerace.Location = New System.Drawing.Point(384, 249)
+        Me.plus1scorerace.Name = "plus1scorerace"
+        Me.plus1scorerace.Size = New System.Drawing.Size(15, 15)
+        Me.plus1scorerace.TabIndex = 6
+        Me.plus1scorerace.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewGameToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 7
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'NewGameToolStripMenuItem
+        '
+        Me.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem"
+        Me.NewGameToolStripMenuItem.Size = New System.Drawing.Size(77, 20)
+        Me.NewGameToolStripMenuItem.Text = "New Game"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 8
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(102, 17)
+        Me.ToolStripStatusLabel1.Text = "Player's Crowns: 0"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(124, 17)
+        Me.ToolStripStatusLabel2.Text = "Opponent's Crowns: 0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 494)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.plus1scorerace)
+        Me.Controls.Add(Me.opponent3)
+        Me.Controls.Add(Me.opponent2)
+        Me.Controls.Add(Me.player3)
+        Me.Controls.Add(Me.player2)
+        Me.Controls.Add(Me.opponent1)
+        Me.Controls.Add(Me.player1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Crown Game"
+        Me.TopMost = True
+        CType(Me.player1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.opponent1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.player2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.player3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.opponent2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.opponent3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.plus1scorerace, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents player1 As PictureBox
+    Friend WithEvents opponent1 As PictureBox
+    Friend WithEvents player2 As PictureBox
+    Friend WithEvents player3 As PictureBox
+    Friend WithEvents opponent2 As PictureBox
+    Friend WithEvents opponent3 As PictureBox
+    Friend WithEvents plus1scorerace As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents NewGameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Hitpoint Game1/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+    Dim selectedPlayer As Integer = 1
+    Dim crownsopponent = 0
+    Dim crownsplayer = 0
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        'Set the Form's KeyPreview property to True
+        If selectedPlayer = 1 Then
+            Select Case e.KeyCode
+                Case Keys.Up
+                    player1.Top = player1.Top - 2
+                Case Keys.Left
+                    player1.Left = player1.Left - 2
+                Case Keys.Right
+                    player1.Left = player1.Left + 2
+                Case Keys.Down
+                    player1.Top = player1.Top + 2
+            End Select
+        ElseIf selectedPlayer = 2 Then
+            Select Case e.KeyCode
+                Case Keys.Up
+                    player2.Top = player2.Top - 2
+                Case Keys.Left
+                    player2.Left = player2.Left - 2
+                Case Keys.Right
+                    player2.Left = player2.Left + 2
+                Case Keys.Down
+                    player2.Top = player2.Top + 2
+            End Select
+        ElseIf selectedPlayer = 3 Then
+            Select Case e.KeyCode
+                Case Keys.Up
+                    player3.Top = player3.Top - 2
+                Case Keys.Left
+                    player3.Left = player3.Left - 2
+                Case Keys.Right
+                    player3.Left = player3.Left + 2
+                Case Keys.Down
+                    player3.Top = player3.Top + 2
+            End Select
+        End If
+
+    End Sub
+
+    Private Sub NewGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewGameToolStripMenuItem.Click
+        Application.Restart()
+    End Sub
+
+    Private Function Chase(ByRef Object1 As Object, ByRef Object2 As Object)
+        If Object1.Left + Object1.Width > Object2.Left Then
+            Object1.Left -= 2
+        End If
+        If Object1.Left + Object1.Width < Object2.Left Then
+            Object1.Left += 2
+        End If
+        If Object1.Top + Object1.Height > Object2.Top Then
+            Object1.Top -= 2
+        End If
+        If Object1.Top + Object1.Height < Object2.Top Then
+            Object1.Top += 2
+        End If
+    End Function
+    Private Function Collision(ByVal Object1 As Object, ByVal Object2 As Object) As Boolean
+        Dim Collided As Boolean = False
+        If Object1.Top + Object1.Height >= Object2.Top And
+                Object2.Top + Object2.Height >= Object1.Top And
+                Object1.Left + Object1.Width >= Object2.Left And
+                Object2.Left + Object2.Width >= Object1.Left Then
+            Collided = True
+        End If
+        Return Collided
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If opponent1.Visible = True Then
+            Chase(opponent1, plus1scorerace)
+        End If
+        If opponent2.Visible = True AndAlso opponent1.Visible = False Then
+            Chase(opponent2, plus1scorerace)
+        End If
+        If opponent3.Visible = True And opponent2.Visible = False And opponent1.Visible = False Then
+            Chase(opponent3, plus1scorerace)
+        End If
+        If Collision(opponent1, plus1scorerace) = True Then
+            opponent1.Visible = False
+            opponent1.Location = New Point(0, 0)
+            crownsopponent += 1
+        End If
+        If Collision(opponent2, plus1scorerace) = True Then
+            opponent2.Visible = False
+            opponent2.Location = New Point(0, 0)
+            crownsopponent += 1
+        End If
+        If Collision(opponent3, plus1scorerace) = True Then
+            Timer1.Enabled = False
+            opponent3.Visible = False
+            opponent3.Location = New Point(0, 0)
+            crownsopponent += 1
+            MsgBox("Defeat! You Lost! Total Crowns: " & crownsplayer & "-" & crownsopponent, MsgBoxStyle.Information, "Opponent Wins!")
+            Application.Restart()
+        End If
+        If Collision(player1, plus1scorerace) = True Then
+            player1.Visible = False
+            player1.Location = New Point(0, 0)
+            selectedPlayer = 2
+            crownsplayer += 1
+        End If
+        If Collision(player2, plus1scorerace) = True Then
+            player2.Visible = False
+            player2.Location = New Point(0, 0)
+            selectedPlayer = 3
+            crownsplayer += 1
+        End If
+        If Collision(player3, plus1scorerace) = True Then
+            Timer1.Enabled = False
+            player3.Visible = False
+            player3.Location = New Point(0, 0)
+            crownsplayer += 1
+            MsgBox("Victory! You Won! Total Crowns: " & crownsplayer & "-" & crownsopponent, MsgBoxStyle.Information, "You Win!")
+            Application.Restart()
+        End If
+        ToolStripStatusLabel2.Text = "Opponent's Crowns: " & crownsopponent
+        ToolStripStatusLabel1.Text = "Player's Crowns: " & crownsplayer
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Move your first player to the green in order for you to score a crown." + Chr(13) + "If you score a crown, your player will disappear and you will move to your second player." + Chr(13) + "If you get 3 crowns, you win.", MsgBoxStyle.Information, "Game Rules")
+        Timer1.Start()
+    End Sub
+End Class
